cef7655009f63d769b79069a25cc0bc0e6a0f8cd
Dmytro <ccccuxoy@gmail.com>
2022-07-08 14:45:45 +0300

    debouce inputs added

--- src/components/exchangeApp/ExchangeApp.js
+++ src/components/exchangeApp/ExchangeApp.js
@@ -1,14 +1,17 @@
 import { useEffect, useState } from "react";
 import { ClockLoader } from "react-spinners";
+import { DebounceInput } from "react-debounce-input";
 import "./exchangeApp.scss";
 
 const ExchangeApp = () => {
   const [currencies, setCurrencies] = useState([]);
   const [loadingStatus, setLoadingStatus] = useState("idle");
   const [mainSelectValue, setMainSelectValue] = useState("");
-  const [secondarySelectValuse, setSecondarySelectValue] = useState("");
+  const [secondarySelectValue, setSecondarySelectValue] = useState("");
   const [mainInputValue, setMainInputValue] = useState(0);
   const [secondaryInputValue, setSecondaryInputValue] = useState(0);
+
+  //Fetch currencies
 
   useEffect(() => {
     setLoadingStatus("loading");
@@ -22,16 +25,29 @@ const ExchangeApp = () => {
       .then(() => setLoadingStatus("idle"));
   }, []);
 
+  //create variables
+
   const currenciesOptions = currencies.map((item) => {
     return <option value={item.cc}>{item.cc}</option>;
   });
-  const mainCurrencyName = currencies.filter((item) => {
-    if (item.cc === mainSelectValue) {
-      return item.txt;
-    }
-  });
-  console.log(mainCurrencyName[0]);
-  console.log(mainSelectValue);
+  const mainCurrencyName =
+    mainSelectValue.length > 0
+      ? currencies.filter((item) => {
+          if (item.cc === mainSelectValue) {
+            return item;
+          }
+        })[0].txt
+      : "";
+  const secondaryCurrencyName =
+    secondarySelectValue.length > 0
+      ? currencies.filter((item) => {
+          if (item.cc === secondarySelectValue) {
+            return item;
+          }
+        })[0].txt
+      : "";
+  console.log(mainInputValue);
+  //render markup + loading spinner / error message
 
   if (loadingStatus === "loading") {
     return (
@@ -57,13 +73,18 @@ const ExchangeApp = () => {
                 </option>
                 {currenciesOptions}
               </select>
-              <div className="currency-converter__item_name">dollar</div>
-              <input
+              <div className="currency-converter__item_name">
+                {mainCurrencyName}
+              </div>
+              <DebounceInput
+                minLength={2}
+                debounceTimeout={300}
                 type="number"
                 name="number"
                 id="number"
                 min={0}
                 className="currency-converter__input"
+                onChange={(e) => setMainInputValue(e.target.value)}
               />
             </div>
             <div className="equal-sign">=</div>
@@ -79,13 +100,18 @@ const ExchangeApp = () => {
                 </option>
                 {currenciesOptions}
               </select>
-              <div className="currency-converter__item_name"></div>
-              <input
+              <div className="currency-converter__item_name">
+                {secondaryCurrencyName}
+              </div>
+              <DebounceInput
+                minLength={2}
+                debounceTimeout={300}
                 type="number"
                 name="number"
                 id="number"
                 min={0}
                 className="currency-converter__input"
+                onChange={(e) => setSecondaryInputValue(e.target.value)}
               />
             </div>
           </div>
